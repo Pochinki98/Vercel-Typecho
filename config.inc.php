5250cf0719f0e001d770437f33602e755c7c9ec6
Pochinki98 <109138085+Pochinki98@users.noreply.github.com>
2023-06-11 03:38:21 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -36,12 +36,14 @@ Typecho_Common::init();
 /** 定义数据库参数 */
 $db = new Typecho_Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
-  'host' => 'mysql.sqlpub.com',
-  'user' => 'vgihijjjij',
-  'password' => 'b6e1c2b5546d81cb',
+  'host' => '数据库地址',
+  'user' => '数据库用户',
+  'password' => '数据库密码',
   'charset' => 'utf8mb4',
   'port' => '3306',
-  'database' => 'huhftfxfg',
+  'database' => '数据库名',
   'engine' => 'MyISAM',
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
+
+
